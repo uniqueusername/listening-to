b9cccb5e8dc6ae3347353e9be634499a532e4c03
ary <7274791+uniqueusername@users.noreply.github.com>
2025-12-14 01:24:11 -0800

    implement text status (song+artist)

--- server/index.js
+++ server/index.js
@@ -1,16 +1,40 @@
 import express from 'express'
 import cors from 'cors'
+import DiscordRPC from 'discord-rpc'
 
 const app = express()
 const port = 2945
+const clientId = '1300015589540106240'
+
+const client = new DiscordRPC.Client({ transport: 'ipc' })
+
+client.on('ready', () => {
+  console.log(`connected to discord client`)
+})
+
+client.login({ clientId }).catch(console.error);
 
 app.use(cors())
 app.use(express.json())
 
 app.post('/', (req, res) => {
+  const { song, artist } = req.body
+  setActivity(song, artist)
   res.send(req.body)
 })
 
 app.listen(port, () => {
   console.log(`listening on port ${port}`)
 })
+
+function setActivity(song, artist) {
+  if (!client) {
+    return;
+  }
+
+  client.setActivity({
+    details: song,
+    state: artist,
+    instance: false,
+  });
+}
